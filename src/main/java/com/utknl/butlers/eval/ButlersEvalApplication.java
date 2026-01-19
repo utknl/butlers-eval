31bd6bcb01712fdbb8e98a2c9f7c844da32a1921
utku <utku.u@freelancer.run>
2026-01-19 09:46:39 +0000

    first commit

---
+++ src/main/java/com/utknl/butlers/eval/ButlersEvalApplication.java
@@ -0,0 +1,13 @@
+package com.utknl.butlers.eval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ButlersEvalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ButlersEvalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utknl/butlers/eval/ButlersEvalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utknl.butlers.eval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ButlersEvalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
